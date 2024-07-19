b22c61cfe2532ca66e4b348e859e0672f851df18
AngshumanPathak <pathakangshuman700@gmail.com>
2024-07-19 14:05:31 +0530

    ColorChanger Done

--- Projects/Project_ColorChanger/chaiaurcode.js
+++ Projects/Project_ColorChanger/chaiaurcode.js
@@ -15,6 +15,23 @@ butttons.forEach(function(button){
 
         body.style.backgroundColor = e.target.id;
     }
+    
+    if(e.target.id === 'yellow'){
+
+        body.style.backgroundColor = e.target.id;
+    }
+
+
+    if(e.target.id === 'white'){
+
+        body.style.backgroundColor = e.target.id;
+    }
+
+
+    if(e.target.id === 'blue'){
+
+        body.style.backgroundColor = e.target.id;
+    }
 
     
    })
