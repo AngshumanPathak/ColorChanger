b3ee18434e162fd224cba26f121419f793e6596d
AngshumanPathak <pathakangshuman700@gmail.com>
2024-07-19 12:41:27 +0530

    Created Color Changer

---
+++ Projects/Project_ColorChanger/chaiaurcode.js
@@ -0,0 +1,25 @@
+const butttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+
+
+butttons.forEach(function(button){
+
+   console.log(button)
+   button.addEventListener('click', function(e){
+
+    console.log(e);
+    console.log(e.target);
+
+    if(e.target.id === 'grey'){
+
+        body.style.backgroundColor = e.target.id;
+    }
+
+    
+   })
+
+})
+
+
+
