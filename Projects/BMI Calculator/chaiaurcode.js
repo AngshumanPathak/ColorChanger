658443e7c2b2826fac094024882be437e2812f65
AngshumanPathak <pathakangshuman700@gmail.com>
2024-07-21 11:42:15 +0530

    BMI Calculator Done

---
+++ Projects/BMI Calculator/chaiaurcode.js
@@ -0,0 +1,45 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e){
+
+    e.preventDefault();
+
+
+const height = parseInt(document.querySelector('#height').value);
+const weight = parseInt(document.querySelector('#weight').value);
+const result = document.querySelector('#results')
+
+
+if(height === ''|| height < 0 || isNaN(height)) {
+
+    result.innerHTML = `Please give a valid height ${height}`;
+}
+
+else if (weight === ''|| weight < 0 || isNaN(weight)) {
+
+    result.innerHTML = `Please give a valid height ${weight}`;
+}
+
+else {
+
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+    result.innerHTML = `<span>BMI: ${bmi}</span></br>`;
+
+    if (bmi < 18.6 ){
+
+        result.innerHTML += `<span> You are underweight</span>`
+    }
+
+    else if (bmi > 18.6 && bmi < 24.9){
+
+        result.innerHTML += `<span> You are normal</span>`
+    } 
+    
+    else {
+
+        result.innerHTML += `<span> You are overweight</span>`
+    }
+}
+
+})
